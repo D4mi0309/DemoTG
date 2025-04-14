d9d51ac209d844207a5dec1e52ff9733b249be05
D4mi0309 <damian.gil01@uceva.edu.co>
2025-04-14 14:40:07 -0500

    Sistema de Autenticación funcionando

---
+++ DemoArTg/Assets/Scripts/EmailAuth.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Auth;
+using TMPro;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    FirebaseAuth auth;
+    [SerializeField] TMP_InputField email;
+    [SerializeField] TMP_InputField password;
+    // Start is called before the first frame update
+    void Start()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+    }
+
+    public void SingUp()
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(email.text, password.text).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            Firebase.Auth.AuthResult result = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                result.User.DisplayName, result.User.UserId);
+        });
+    }
+
+    public void LogIn()
+    {
+        auth.SignInWithEmailAndPasswordAsync(email.text, password.text).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.AuthResult result = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                result.User.DisplayName, result.User.UserId);
+        });
+    }
+}
